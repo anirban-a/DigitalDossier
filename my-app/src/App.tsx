f5392a65f325d76aeb936239aa3c545ca86fba6d
Anirban Acharya <acharya.anirban9@gmail.com>
2024-01-02 12:15:48 -0500

    initial refactor

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,15 @@
 import "./App.css";
-import { AppBar } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
-import AppTitle from "./components/AppTitle";
-import SideBar from "./components/SideBar";
+import MenuBar from "./components/MenuBar";
 
 function App() {
+  // const [menuOpen, setMenuOpen] = useState(true);
+  // const handleMenuOpen = () => {
+  //   setMenuOpen(!menuOpen);
+  // };
+
   return (
     <>
-      <AppBar color="primary">
-        <Stack direction="row">
-          <Avatar
-            variant="rounded"
-            alt="Logo"
-            src="/images/uifaces-cartoon-image.jpg"
-            sx={{ width: 56, height: 58 }}
-            className="logo"
-          />
-          <AppTitle>Anirban Acharya</AppTitle>
-        </Stack>
-      </AppBar>
-      <SideBar>{["About", "Skills", "Experience", "My Notes"]}</SideBar>
+      <MenuBar />
     </>
   );
 }
