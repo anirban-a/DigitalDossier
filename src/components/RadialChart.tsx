c52852d83f28e0fe4fb3e4785ecd0585f3462076
Anirban Acharya <acharya.anirban9@gmail.com>
2024-01-05 15:16:54 -0500

    removed apexcharts

--- src/components/RadialChart.tsx
+++ src/components/RadialChart.tsx
@@ -1,100 +1,43 @@
-import Chart from "react-apexcharts";
+import {
+  RadarChart,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  PolarAngleAxis,
+} from "recharts";
 
 interface ChartProps {
   data: number[];
   labels: string[];
 }
 
+function formatData(values: number[], labels: string[]) {
+  return values.map((v, idx) => {
+    return { skill: labels[idx], level: v };
+  });
+}
+
 export default function RadialChart({ data, labels }: ChartProps) {
-  const state = {
-    chart: {
-      type: "polarArea",
-      series: data,
-      // chart options here:
-      options: {
-        labels: labels,
-        outerHeight: 100,
-        colors: [
-          "#d47718",
-          "#ff3b0d",
-          "#3be1c8",
-          "#e3db42",
-          "#3b54e1",
-          "#2E93fA",
-          "#66DA26",
-          "#546E7A",
-          "#E91E63",
-          "#00849d",
-          "#ff3b0d",
-          "#ec54c8",
-          "#e272af",
-          "#ff7f00",
-          "#68f84d",
-          "#43BCCD",
-          "#5500c3",
-          "#c100e0",
-          "#53777A",
-          "#FF7F50",
-          "#C0C0C0",
-          "#9B59B6",
-          "#2ECC71",
-          "#FEB019",
-          "#775DD0",
-          "#7D02EB",
-        ],
-        theme: {
-          mode: "dark",
-          palette: "palette8",
-          monochrome: {
-            enabled: false,
-            color: "#3c3c3c",
-            shadeTo: "light",
-            shadeIntensity: 0.55,
-          },
-        },
-        markers: {
-          width: 12,
-          height: 12,
-          strokeWidth: 0,
-          strokeColor: "#3c3c3c",
-          //   fillColors: undefined,
-          radius: 12,
-          //   customHTML: undefined,
-          //   onClick: undefined,
-          offsetX: 0,
-          offsetY: 0,
-        },
-        onItemHover: {
-          highlightDataSeries: true,
-        },
-        chart: {
-          fontFamily: "Helvetica",
-          fontSize: 40,
-          background: "#262626",
-          animations: {
-            enabled: true,
-            easing: "easeinout",
-            speed: 800,
-            animateGradually: {
-              enabled: true,
-              delay: 150,
-            },
-            dynamicAnimation: {
-              enabled: true,
-              speed: 350,
-            },
-          },
-        },
-      },
-    },
-  };
+  const formattedDataList = formatData(data, labels);
+  console.log(formattedDataList);
+
   return (
-    <Chart
-      options={state.chart.options}
-      series={state.chart.series}
-      type={"polarArea"}
-      width={700}
-      height={1000}
-    />
+    <RadarChart
+      outerRadius={150}
+      width={730}
+      height={350}
+      data={formattedDataList}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="skill" />
+      <PolarRadiusAxis angle={45} domain={[0, 4]} />
+      <Radar
+        name="Skills"
+        dataKey="level"
+        stroke="#e23008"
+        fill="#e4b517"
+        fillOpacity={0.6}
+      />
+    </RadarChart>
   );
 }
